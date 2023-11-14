e03a922e2c564958a05b7f5e5e8fff724f59d0fa
khot-aditya <adityakhot6838@gmail.com>
2023-11-14 23:56:19 +0530

    vite config modification

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr()],
+  plugins: [react(), svgr()],
+  base: '/mui-theme-generator/', // Set the base path for your project
 })
