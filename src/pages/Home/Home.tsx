f6627e7b964ad9561eacd1c88b5c067fc8012757
khot-aditya <adityakhot6838@gmail.com>
2023-11-15 14:36:46 +0530

    added editor and home page contents

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,57 @@
+import { Box, Button, Typography } from "@mui/material"
+import { Container } from "./styles"
+import { useNavigate } from "react-router-dom"
+import { routes } from "../../constants/Constants";
 
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleGetStartedClick = () => {
+        navigate(routes.generator.index)
+    }
 
     return (
-        <>
-            Home
-        </>
+        <Container>
+            <Box
+                width={'100%'}
+                maxWidth={'800px'}
+            >
+                <Typography
+                    fontSize={60}
+                    whiteSpace={"nowrap"}
+                    sx={{
+                        verticalAlign: 'start'
+                    }}
+                    variant="h1">
+                    MUI Theme Generator
+                    <Typography
+                        fontWeight={"600"}
+                        component={'span'}
+                        fontSize={25}> (Beta)</Typography>
+                </Typography>
+
+                <Typography
+                    variant="body1"
+                >
+                    MUI Theme Generator is a powerful tool that allows users to create, edit, and visualize themes for the Material-UI (MUI) component library. With real-time preview capabilities and a user-friendly interface, designing and customizing MUI themes becomes a seamless experience.
+                </Typography>
+            </Box>
+
+            <Box display={'flex'} gap={1}>
+                <Button
+                    href="https://github.com/khot-aditya/mui-theme-generator"
+                    variant="outlined"
+                >
+                    Github
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={handleGetStartedClick}>
+                    Get Started
+                </Button>
+            </Box>
+        </Container>
     )
 }
 
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import styled from "styled-components";
+
+
+export const Container = styled(Box)`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+    align-items: center;
+    justify-content: center;
+    
+`
