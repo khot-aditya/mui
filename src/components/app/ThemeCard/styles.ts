2e1f815bfe1f5f35d60db5e24285407b717633e5
Aditya Khot <adityakhot6838@gmail.com>
2023-11-27 23:17:16 +0530

    Fix border radius and bookmark icon in ThemeCard component

--- src/components/app/ThemeCard/styles.ts
+++ src/components/app/ThemeCard/styles.ts
@@ -1,11 +1,23 @@
 import { Box, styled } from "@mui/material";
+import { BookmarkBoxProps } from "./ThemeCard.types";
 
 
 export const Card = styled(Box)(({ theme }) => ({
-    borderRadius: theme.spacing(1),
+    borderRadius: 20,
     backgroundColor: theme.palette.background.paper,
     transition: theme.transitions.create("box-shadow"),
     border: `1px solid ${theme.palette.divider}`,
     minWidth: 200,
-    // maxWidth: 300,
+    boxShadow: "3px 5px 10px rgba(0, 0, 0, 0.04)",
 }));
+
+export const BookmarkBox = styled(Box)
+    <BookmarkBoxProps>(({ isBookmarked }) => ({
+        "& svg": {
+            fill: '#747474',
+            transition: 'fill 0.2s ease-in-out',
+        },
+        "&:hover svg": {
+            fill: isBookmarked ? '#ff2828' : '#000',
+        }
+    }));
